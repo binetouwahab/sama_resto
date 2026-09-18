bda7b9dc9b66f2fd9db69005f2f025c9b2394947
binetouwahab <abwnd15@gmail.com>
2026-09-18 20:12:49 +0000

    créer le composant CustomTextField réutilisable

---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String hint;
+  final IconData icon;
+  final bool obscure;
+  final TextEditingController controller;
+  final TextInputType keyboardType;
+
+  const CustomTextField({
+    super.key,
+    required this.hint,
+    required this.icon,
+    required this.controller,
+    this.obscure = false,
+    this.keyboardType = TextInputType.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 6),
+      child: TextField(
+        controller: controller,
+        obscureText: obscure,
+        keyboardType: keyboardType,
+        decoration: InputDecoration(
+          prefixIcon: Icon(icon, color: const Color(0xFFC0202D)),
+          hintText: hint,
+          filled: true,
+          fillColor: Colors.white,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: Colors.grey.shade300),
+          ),
+        ),
+      ),
+    );
+  }
+}
